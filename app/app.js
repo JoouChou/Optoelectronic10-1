0d23df729c4fb710ef6769edd128e170c81102c1
luckyjoou <luckyjoou@gmail.com>
2015-08-18 10:40:26 +0800

    first pages commit

---
+++ app/app.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+	var oneDay = 24*60*60*1000; 
+	var classDay = new Date(2014,06,30);
+	var Today = new Date();
+
+	var diffDay = Math.round(( Today.getTime() - classDay.getTime()) / oneDay);
+
+	$('.num').append(diffDay);
+
+	console.log(diffDay);
+
+});
